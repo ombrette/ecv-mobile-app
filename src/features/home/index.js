95a62d532db554698e87b4ee06a17c0d3fb140ef
ombrette <lamberthema@hotmail.fr>
2018-05-15 22:16:05 +0200

    Add basic components as home & movie list

---
+++ src/features/home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = ({ navigation }) => (
+    <View>
+        <Text>Home page</Text>
+    </View>
+);
+
+export default Home;
