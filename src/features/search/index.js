05f4cef50153092ef06457f6956892d0155da53f
ombrette <lamberthema@hotmail.fr>
2018-05-20 21:13:18 +0200

    Add search component

--- src/features/search/index.js
+++ src/features/search/index.js
@@ -1,40 +1,67 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, AsyncStorage, StyleSheet, Image } from 'react-native';
 import Config from 'react-native-config';
+
+const styles = StyleSheet.create({
+ 
+MainContainer :{
+ 
+    justifyContent: 'center',
+    flex:1,
+    margin: 5,
+    marginTop: 20,
+ 
+},
+ 
+// imageView: {
+ 
+//     width: '50%',
+//     height: 100 ,
+//     margin: 7,
+//     borderRadius : 7
+
+// },
+ 
+textView: {
+ 
+    width:'50%', 
+    textAlignVertical:'center',
+    padding:10,
+    color: '#000'
+ 
+}
+});
 
 export default class Search extends React.Component {
 
     constructor(props){
         super(props);
-        this.state ={ isLoading: true}
+        this.state ={ 
+            isLoading: true,
+        }
     }
 
-    componentDidMount(){
-        return fetch('https://api.themoviedb.org/3/discover/movie?&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1', {
+    componentDidMount() {
+        return fetch('http://192.168.1.37:3000/api/search/{"params": "movie"}', {
             method: "GET",
-            headers: {
-                'Authorization': 'Bearer ' + Config.API_TOKEN
-            }
         })
-            .then((response) => response.json())
-            .then((responseJson) => {
+        .then((response) => response.json())
+        .then((responseJson) => {
 
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson.results,
-                }, function(){
-
-                });
-
-            })
-            .catch((error) =>{
-                console.error(error);
+            this.setState({
+                isLoading: false,
+                movies: responseJson.movies,
             });
+
+        })
+        .catch((error) =>{
+            console.error(error);
+        });
+
     }
 
 
     render(){
-
         if(this.state.isLoading){
             return(
                 <View style={{flex: 1, padding: 20}}>
@@ -46,8 +73,16 @@ export default class Search extends React.Component {
         return(
             <View style={{flex: 1, paddingTop:20}}>
                 <FlatList
-                    data={this.state.dataSource}
-                    renderItem={({item}) => <Text>{item.title}</Text>}
+                    data={this.state.movies}
+                    renderItem={({item}) => 
+                        <View style={{flex:1, flexDirection: 'row'}}>
+        
+                          <Image source = {{ uri: "https://image.tmdb.org/t/p/w185_and_h278_bestv2" + item.poster_path }} />
+                        
+                          <Text style={styles.textView} >{item.title}</Text>
+             
+                        </View>
+                    }
                     keyExtractor={(item, index) => index}
                 />
             </View>
